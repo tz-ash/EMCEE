bba11214bab5a4735f657251206ac47262f484dc
satyanandatripathi <83153745+satyanandatripathi@users.noreply.github.com>
2021-05-14 12:05:02 +0530

    Update logomaker.py

--- MashaRoBot/modules/logomaker.py
+++ MashaRoBot/modules/logomaker.py
@@ -37,7 +37,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByEmcee.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Created By Emcee✨")
+    await tbot.send_file(event.chat_id, fname2, caption="Created By Haguromo Otsusuki")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
@@ -77,7 +77,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "LogoByEmcee.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Created By Emcee")
+    await tbot.send_file(event.chat_id, fname2, caption="Created By Haguromo Otsusuki")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
